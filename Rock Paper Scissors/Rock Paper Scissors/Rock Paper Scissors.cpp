89c25abb6f988b3b0cd64f0c5c08eb5fcd926bd0
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2021-02-09 18:34:02 +0300

    :100:

--- Rock Paper Scissors/Rock Paper Scissors/Rock Paper Scissors.cpp
+++ Rock Paper Scissors/Rock Paper Scissors/Rock Paper Scissors.cpp
@@ -1,20 +1,29 @@
 // Rock Paper Scissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+/*
+* ROOM FOR IMPROVEMENT
+* Limit the number of games played e.g 3 or 5 rounds.
+* Display the scores for longer games
+*/
+
 #include <iostream>
 #include <stdlib.h>
 #include<time.h>
 using namespace std;
 
-
+// Array holding the choices, for the computer to randomly pick.
 char choices[3] = {'R','P','S'};
 
+// Main function that runs the game.
 void game() {
     bool run = true;
-    char userSelection;
-    char computerSelection;
-    int games;
+    char userSelection; // Store the user's choice
+    char computerSelection; // Store the computer's choice
+
+    // Game loop.
     while (run) {
+        // User Play!
         cout << "Select Rock[R], Paper[P] or Scissors[S]" << endl;
         cin >> userSelection;
 
@@ -26,7 +35,6 @@ void game() {
         if (((computerSelection == 'R') && (userSelection == 'S')) || ((computerSelection == 'P') && (userSelection == 'R')) || ((computerSelection == 'S') && (userSelection == 'P'))) {
             cout << "You chose : " << userSelection << "while the Computer Chose : " << computerSelection << endl;
             cout << "You Lose !!!" << endl;
-           
         }
         else if (computerSelection == userSelection) {
             cout << "You Chose : " << userSelection << " and Computer chose : " << computerSelection << endl;
@@ -47,6 +55,7 @@ int main()
     // Random Seed
     srand(time(NULL));
 
-    cout << "Hello World!\n";
+    cout << "Hello World! Let's Play Rock-Paper-Scissors\n";
+    // Run the game
     game();
 }
